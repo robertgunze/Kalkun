e60f92ae80634e3e6665ef5c52f9e50ec59e7f22
Robert Gunze <robertgunze@gmail.com>
2021-11-20 10:55:22 +0300

    Initialized sms_to_webhook plugin

---
+++ application/plugins/sms_to_webhook/config/sms_to_webhook.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| CONFIGURATION
+|--------------------------------------------------------------------------
+|
+| webhook_url - Webhook URL (Don't use space)
+|
+*/
+$config['webhook_url'] = 'https://app.shambaplus.com/api/SMS/text';
+
+/* End of file sms_to_webhook.php */
+/* Location: ./application/plugins/sms_to_webhook/config/sms_to_webhook.php */
