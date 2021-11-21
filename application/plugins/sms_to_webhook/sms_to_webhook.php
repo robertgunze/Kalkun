3ea9f94176beefd955db763f5dd625c2cb1d2b73
Robert Gunze <robertgunze@gmail.com>
2021-11-21 10:45:04 +0300

    modifed sms_to_webhook hook in sms_to_webhook plugin

--- application/plugins/sms_to_webhook/sms_to_webhook.php
+++ application/plugins/sms_to_webhook/sms_to_webhook.php
@@ -88,7 +88,7 @@ function sms_to_webhook($sms)
     //$CI->load->model('sms_to_webhook/sms_to_webhook_model', 'plugin_model');
     $CI->load->library('sms_to_webhook/webhook', 'webhook');
     
-    $response = $CI->webhook->get($webhook_url, array('phone' => $number, 'text' => urlencode($message), 'smscenter'=> $smscenter));
+    $response = $CI->webhook->get($webhook_url, ['phone' => urlencode($number), 'text' => urlencode($message), 'smscenter'=> urlencode($smscenter)]);
 	print_r($response);
     //autoreply($sms, $response);
 	$CI->webhook->get("http://localhost/index.php/plugin/rest_api/send_sms?phoneNumber=$number&message=".urlencode($response));
