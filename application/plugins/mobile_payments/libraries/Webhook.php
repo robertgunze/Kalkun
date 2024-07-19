e3f317e480bb35ab15d07e7429e2f91d44e79ed5
Robert Makoye <robertgunze@gmail.com>
2024-07-19 11:12:56 +0300

    Added mobile_payments plugin

---
+++ application/plugins/mobile_payments/libraries/Webhook.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+ * simple http client helper
+ */
+class Webhook {
+
+    function __construct() {
+
+	}
+
+    public function get ($url, $params = []) {
+        $conn = curl_init();
+        if ( is_array($params) && !empty($params) )
+		{
+			$url = $url . '?' . $this->_params_to_query($params);
+		}
+
+        curl_setopt($conn, CURLOPT_URL, $url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
+
+        $response = curl_exec($conn);
+        curl_close($conn);
+
+        return $response;
+    }
+
+    private function _params_to_query($params)
+	{
+		if ( !is_array($params) || empty($params) )
+		{
+			return '';
+		}
+
+		$query = '';
+
+		foreach	( $params as $key => $value )
+		{
+			$query .= $key . '=' . $value . '&';
+		}
+
+		return substr($query, 0, strlen($query) - 1);;
+	}
+}
